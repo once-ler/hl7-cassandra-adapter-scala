7f3b90206065ac7d39fe45b09778eeae38cad469
once-ler <htao@eztier.com>
2018-07-31 13:36:12 -0400

    Start to break up persistence routine to granular stages

--- src/main/scala/stream/CassandraPersistenceTraits.scala
+++ src/main/scala/stream/CassandraPersistenceTraits.scala
@@ -1,11 +1,14 @@
 package com.eztier.stream.traits
 
-import akka.NotUsed
-import akka.stream.SourceShape
-import akka.stream.scaladsl.{Flow, GraphDSL, Keep, Source}
+import java.util.Date
+
+import akka.{NotUsed, stream}
+import akka.stream.{FlowShape, SinkShape, SourceShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, Sink, Source}
 import com.datastax.driver.core.Row
 import com.eztier.hl7mock.{CaBase, CaControl}
 import com.eztier.stream.CommonTask.balancer
+import com.sun.corba.se.impl.orbutil.graph.Graph
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -14,6 +17,29 @@ import scala.reflect.runtime.universe.{TypeTag, typeOf}
 trait WithCassandraPersistence[A <: CaBase, B <: CaControl] extends WithHapiToCassandraFlowTrait[A, B] {
   // Type-class implicits
   import com.eztier.hl7mock._
+
+  def persistToCassandraStage(workerCount: Int = 10)
+  (implicit caToCaControlConverter: CaToCaControl[A, B], baseConverter: CaInsertStatement[A], controlConverter: CaInsertStatement[B], typeTag: TypeTag[B]) = {
+    GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      val bcast = b.add(Broadcast[Option[A]](1))
+      val save = b.add(balancer(persist, workerCount))
+      val gather = b.add(Flow[Date].grouped(100000))
+      val controlUpdate = b.add(updateDateControl(typeOf[B].typeSymbol.name.toString))
+
+      bcast ~> save ~> gather ~> controlUpdate
+
+      FlowShape(bcast.in, controlUpdate.out)
+    }
+  }
+
+  def persistToCassandraFrom(s: Source[Option[A], NotUsed], workerCount: Int = 10)
+  (implicit caToCaControlConverter: CaToCaControl[A, B], baseConverter: CaInsertStatement[A], controlConverter: CaInsertStatement[B], typeTag: TypeTag[B]) = {
+    val persistence = Flow.fromGraph(persistToCassandraStage(workerCount))
+
+    val g = s.via(persistence).toMat(sumSink)(Keep.right)
+  }
 
   def persistToCassandra(s: Source[Option[A], NotUsed], workerCount: Int = 10)
   (implicit caToCaControlConverter: CaToCaControl[A, B], baseConverter: CaInsertStatement[A], controlConverter: CaInsertStatement[B], typeTag: TypeTag[B]) = {
