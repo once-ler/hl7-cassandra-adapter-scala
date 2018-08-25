e49126cd4f3752d37b6c7baeba977903dba8e440
once-ler <htao@eztier.com>
2018-08-25 07:40:41 -0400

    Complete create patient trait with exception handling

--- src/main/scala/hl7mock/common/CaCreateTypes.scala
+++ src/main/scala/hl7mock/common/CaCreateTypes.scala
@@ -1,41 +1,47 @@
 package com.eztier.hl7mock
 
-import akka.stream.scaladsl.{Flow, Source}
+import akka.event.LoggingAdapter
 import com.datastax.driver.core.SimpleStatement
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 import com.eztier.cassandra.CaCommon.getCreateStmt
 import com.eztier.cassandra.CaCustomCodecProvider
 import com.eztier.hl7mock.types._
 
 trait CaCreateTypes[A <: CaBase, B <: CaControl] {
-  import com.eztier.cassandra.CaCommon._
+  import com.eztier.cassandra.CaCommon.getCreateStmt
 
   // Create the UDTs and Tables before handing over to the registrar.
-  def create(provider: CaCustomCodecProvider): CaCustomCodecProvider
+  def create(provider: CaCustomCodecProvider)(implicit logger: LoggingAdapter): Either[Exception, Int]
 }
 
 object CaCreateTypes {
   // CaPatient
   implicit object CreateCaPatient extends CaCreateTypes[CaPatient, CaPatientControl] {
-    override def create(provider: CaCustomCodecProvider): CaCustomCodecProvider = {
 
-      Source.single(1)
-        .via(Flow[Int].mapAsync(1){_ => provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientPhoneInfo])) })
+    override def create(provider: CaCustomCodecProvider)(implicit logger: LoggingAdapter): Either[Exception, Int] = {
+      val r = {
+        for {
+          a <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientPhoneInfo]))
+          b <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientEmailInfo]))
+          c <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientIdType]))
+          d <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientNameComponents]))
+          e <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientAddress]))
+          f <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientCareTeamMember]))
+          g <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientEmergencyContact]))
+          h <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientEmploymentInformation]))
+          i <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatient]))
+          j <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientControl]))
+        } yield Right(1)
+      }.recover {
+        case e: Exception => logger.error(e.getMessage)
+        Left(e)
+      }
 
-      val r = for {
-        a <- provider.readAsync(new SimpleStatement(getCreateStmt[CaPatientPhoneInfo]))
-        b <- provider.session.executeAsync(getCreateStmt[CaPatientEmailInfo])
-        c <- provider.session.executeAsync(getCreateStmt[CaPatientIdType])
-        d <- provider.session.executeAsync(getCreateStmt[CaPatientNameComponents])
-        e <- provider.session.executeAsync(getCreateStmt[CaPatientAddress])
-        f <- provider.session.executeAsync(getCreateStmt[CaPatientCareTeamMember])
-        g <- provider.session.executeAsync(getCreateStmt[CaPatientEmergencyContact])
-        h <- provider.session.executeAsync(getCreateStmt[CaPatientEmploymentInformation])
-        i <- provider.session.executeAsync(getCreateStmt[CaPatient])
-        j <- provider.session.executeAsync(getCreateStmt[CaPatientControl])
-      } yield 1
-
-
-
+      val s = Await.result(r, 30 seconds)
+      s
     }
+
   }
 }
