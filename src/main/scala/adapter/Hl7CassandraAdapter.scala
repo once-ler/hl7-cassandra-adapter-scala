e0471a79bd7d7ff9811441cd4e9a4bcafd04bd8a
once-ler <htao@eztier.com>
2018-09-01 16:43:14 -0400

    Adapter should require implicit cassandra type creator

--- src/main/scala/adapter/Hl7CassandraAdapter.scala
+++ src/main/scala/adapter/Hl7CassandraAdapter.scala
@@ -1,13 +1,14 @@
 package com.eztier.adapter
 
 import java.io.{PrintWriter, StringWriter}
+
 import akka.actor.ActorSystem
 import com.eztier.cassandra.CaCustomCodecProvider
-import com.eztier.hl7mock.{CaBase, CaControl, CaRegisterUdt}
+import com.eztier.hl7mock.{CaBase, CaControl, CaCreateTypes, CaRegisterUdt}
 import com.eztier.stream.HapiToCassandraFlowTask
 
 class Hl7CassandraAdapter[A <: CaBase, B <: CaControl](provider: CaCustomCodecProvider, keySpace: String)
-  (implicit registrar: CaRegisterUdt[A, B]){
+  (implicit creator: CaCreateTypes[A, B], registrar: CaRegisterUdt[A, B]){
 
     val flow = HapiToCassandraFlowTask[A, B](provider, keySpace)
 
@@ -17,7 +18,7 @@ object Hl7CassandraAdapter {
   private implicit val system = ActorSystem("Sys")
   private implicit val logger = system.log
 
-  def apply[A <: CaBase, B <: CaControl](configPath: String, keySpace: String)(implicit registrar: CaRegisterUdt[A, B]) = {
+  def apply[A <: CaBase, B <: CaControl](configPath: String, keySpace: String)(implicit creator: CaCreateTypes[A, B], registrar: CaRegisterUdt[A, B]) = {
     val maybeProvider: Either[String, CaCustomCodecProvider] = try {
       Right(CaCustomCodecProvider(configPath))
     } catch {
